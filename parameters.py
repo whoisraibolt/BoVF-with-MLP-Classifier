a8fae0eb1d752837b92314eb94d09b88ae8297bb
Alexandra Raibolt <alexandra.raibolt@gmail.com>
2020-05-13 16:30:50 -0300

    Initial commit parameters.py

---
+++ parameters.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Set Multilayer Perceptron parameters
+
+# Avoid non-deteministic behaviour
+seed = 2019
+
+# Learning Rate 
+learning_rate = 0.01
+
+# L2 Regularization
+alpha = 0.0001
+
+# Maximum number of iterations
+max_iterations = 5000
+
+# Hidden Layer Sizes
+hidden_layer = (50, 30, 20, 20, 20, 30, 50)
